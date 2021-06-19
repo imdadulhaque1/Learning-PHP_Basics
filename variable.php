a9933fe1692afe9faecc444221463cf431967451
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-06-19 10:37:13 +0600

    Constant varibale in PHP

---
+++ variable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Variable in PHP</title>
+</head>
+<body>
+          <?php
+                    // Constant Variable
+                    define('name',"Imdadul Haque");         #By default it takes the boolean value as false
+                    echo "My Name is ".name."<br>";
+
+                    define('s_id',1440, true);
+                    echo "My Name is ".S_ID;
+          ?>
+</body>
+</html>
