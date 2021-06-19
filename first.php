2ed062f4bb08879f44feb9b9dcb1f3f8a00cc212
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-06-19 10:35:48 +0600

    PHP first code

---
+++ first.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>PHP first Code</title>
+</head>
+<body>
+          <?php
+                    $name ="Imdadul Haque";
+                    $lastFourID = 1440;
+                    echo "Hey, myself ". $name." and my student's last four ID is ".$lastFourID.".";
+          ?>
+</body>
+</html>
