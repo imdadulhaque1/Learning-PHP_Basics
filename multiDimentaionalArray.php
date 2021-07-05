3e951268e96db84595a8b9887de920b0629bb71e
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-07-05 21:27:19 +0600

    Multi Dimentional Array's elements prints using forEach loop

--- multiDimentaionalArray.php
+++ multiDimentaionalArray.php
@@ -8,9 +8,16 @@ $arr = [
           [5, "Shampa Khatun", "Female", "Student"]
 ];
 
-for($row=0; $row<5; $row++){
-          for($col=0; $col<4; $col++){
-                    echo $arr[$row][$col]." ";
+// for($row=0; $row<5; $row++){
+//           for($col=0; $col<4; $col++){
+//                     echo $arr[$row][$col]." ";
+//           }
+//           echo "<br>";
+// }
+
+foreach ($arr as $v1){
+          foreach($v1 as $v2){
+                    echo $v2." ";
           }
           echo "<br>";
 }
