4bd8c892faaf62a539d8bd0e42c8998cc1ba999e
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-07-05 23:15:49 +0600

    MultiDimentional Associative Array in table using forEach loop

--- MultiDimentionalAssociativeArray.php
+++ MultiDimentionalAssociativeArray.php
@@ -33,8 +33,26 @@ $marks=[
           ],
 ];
 
-echo "<pre>";
-print_r($marks);
-echo "</pre>";
+echo "<table border='2px' cellpadding='5px' cellspacing='0px'>";
+echo "<tr>
+          <th>Student Name</th>
+          <th>Programming</th>
+          <th>Artificial Intelligence</th>
+          <th>Data Mining</th>
+          <th>Embedded Systems</th>
+</tr>";
+foreach($marks as $key => $v1){
+          echo "<tr>
+                    <td>$key</td>";
+          foreach($v1 as $v2){
+                    echo "<td>$v2</td>";
+          }
+          echo "</tr>";
+
+}
+echo "</table>";
+// echo "<pre>";
+// print_r($marks);
+// echo "</pre>";
 
 ?>
