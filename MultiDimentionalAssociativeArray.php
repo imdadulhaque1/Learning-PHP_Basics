601e12bca013d778f3d4d1f4c81184220f31fe9f
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-07-05 22:30:16 +0600

    MultiDimentional Associative Array

---
+++ MultiDimentionalAssociativeArray.php
@@ -0,0 +1,40 @@
+<?php
+
+$marks=[
+          "Imdadul Haque" =>[
+                    "Programming" => 75,
+                    "Artificial Intelligence" => 79,
+                    "Data Mining" => 80,
+                    "Embedded Systems" =>75
+          ],
+          "Md. Suhel Rana" =>[
+                    "Programming" => 85,
+                    "Artificial Intelligence" => 81,
+                    "Data Mining" => 70,
+                    "Embedded Systems" =>78
+          ],
+          "Mizanur Rahman" =>[
+                    "Programming" => 83,
+                    "Artificial Intelligence" => 77,
+                    "Data Mining" => 81,
+                    "Embedded Systems" =>88
+          ],
+          "Shamima Akter" =>[
+                    "Programming" => 84,
+                    "Artificial Intelligence" => 85,
+                    "Data Mining" => 80,
+                    "Embedded Systems" =>87
+          ],
+          "Md. Lakib Hasan" =>[
+                    "Programming" => 85,
+                    "Artificial Intelligence" => 80,
+                    "Data Mining" => 78,
+                    "Embedded Systems" =>77
+          ],
+];
+
+echo "<pre>";
+print_r($marks);
+echo "</pre>";
+
+?>
