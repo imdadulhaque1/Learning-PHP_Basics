114547672b9b57d2e026d527b51229e8e5e4cf55
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-07-05 18:41:10 +0600

    while loop

---
+++ whileLoop.php
@@ -0,0 +1,12 @@
+<?php
+
+$status  = "I love my Country, Bangladesh!";
+$i = 1;
+echo "<ul>";
+while ($i <= 10){
+          echo "<li>"." ".$status." ".$i."</li> ";
+          $i++;
+}
+echo "</ul>";
+
+?>
