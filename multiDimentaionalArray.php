512254cf696ee586377fe699598ae7919e030c6f
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-07-05 21:17:12 +0600

    Multi Dimentional Array's elements prints using for loop

--- multiDimentaionalArray.php
+++ multiDimentaionalArray.php
@@ -8,21 +8,19 @@ $arr = [
           [5, "Shampa Khatun", "Female", "Student"]
 ];
 
-echo $arr[0][0]."   ";
-echo $arr[0][1]."   ";
-echo $arr[0][2]."   ";
-echo $arr[0][3]."   "."<br>";
-echo $arr[1][0]."   ";
-echo $arr[1][1]."   ";
-echo $arr[1][2]."   ";
-echo $arr[1][3]."   "."<br>";
-echo $arr[4][0]."   ";
-echo $arr[4][1]."   ";
-echo $arr[4][2]."   ";
-echo $arr[4][3]."   "."<br>";
+for($row=0; $row<5; $row++){
+          for($col=0; $col<4; $col++){
+                    echo $arr[$row][$col]." ";
+          }
+          echo "<br>";
+}
 
-echo "<pre>";
-print_r($arr);
-echo "</pre>";
+// echo $arr[0][0]."   ";
+// echo $arr[0][1]."   ";
+// echo $arr[0][2]."   ";
+// echo $arr[0][3]."   "."<br>";
+// echo "<pre>";
+// print_r($arr);
+// echo "</pre>";
 
 ?>
